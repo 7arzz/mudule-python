b9bcc3a21414e9fce50f0463ba7ba9c448c1c18d
7arzz <7arzz404@gmail.com>
2025-11-21 19:01:27 +0700

    edit tugas mandiri

--- tugasMandiri.py
+++ tugasMandiri.py
@@ -1,6 +1,7 @@
 # ============================================
 # GAME GUNTING BATU KERTAS
 # ============================================
+print("=== Game Gunting Batu Kertas (Sedikit absurd) ===")
 
 def hasil(p1, p2):
     if p1 == p2:
@@ -80,7 +81,10 @@ for i in range(1, n + 1):
 
 list1 = [13, 15, 30, 41, 45, 55, 75, 95, 102, 135, 139, 151, 193, 200]
 
-print("Angka yang habis dibagi 3 dan <=151:")
+print("""
+      Angka yang habis dibagi 3 dan <=151: 
+      dari list1 = [13, 15, 30, 41, 45, 55, 75, 95, 102, 135, 139, 151, 193, 200]
+      """)
 
 for x in list1:
     if x % 3 == 0 and x <= 151:
